17812770451e5647bd29587ef8b79c18645fe09c
uqee <denis@zhbankov.com>
2023-07-21 21:36:05 +0000

    1 file modified

--- src/Node.ts
+++ src/Node.ts
@@ -5,6 +5,8 @@ import { inspect } from './inspect';
 import { NodeStatus } from './NodeStatus';
 // import { NodeType } from './NodeType';
 
+//
+
 type NodeParams = {
   bal?: number;
   comment?: string;
@@ -20,52 +22,65 @@ type NodeParams = {
   [key: string]: unknown;
 };
 
+const NodeParams_knownKeys: (keyof NodeParams)[] = [
+  'bal',
+  'comment',
+  'dps',
+  'id',
+  'name',
+  'pnl',
+  'root',
+  'status',
+  // 'type',
+  'wds',
+];
+
+//
+
 export class Node {
   public static fromRetool(): Node {
     throw new Error();
   }
 
+  private static getContent_lengthHeader: number = 21;
+  private static getContent_lengthKey: number = 9;
   private static getContent(params: NodeParams): string {
     let content: string = '';
 
     // header
 
-    content += `${params.id}`.padEnd(7, ' ');
+    content += `🧑${params.id}`.padEnd(Node.getContent_lengthKey, ' ');
     if (params.status !== undefined) content += ` | ${params.status}`;
-    content += '\n' + '-'.repeat(18);
+    content += `\n${'-'.repeat(Node.getContent_lengthHeader)}`;
 
     // body
     // known params
 
     if (params.name !== undefined) {
-      content += `\n${'name'.padEnd(7, ' ')} | ${params.name}`;
+      content +=
+        `\n${'name'.padEnd(Node.getContent_lengthKey, ' ')}` +
+        ` | ${params.name}`;
     }
 
     if (params.pnl !== undefined || params.balance !== undefined) {
-      content += `\n${'pnl/bal'.padEnd(7, ' ')} | ${params.pnl}/${params.bal}`;
+      content +=
+        `\n${'pnl/bal'.padEnd(Node.getContent_lengthKey, ' ')}` +
+        ` | ${params.pnl}/${params.bal}`;
     }
 
     if (params.dps !== undefined || params.wds !== undefined) {
-      content += `\n${'dps/wds'.padEnd(7, ' ')} | ${params.dps}/${params.wds}`;
+      content +=
+        `\n${'dps/wds'.padEnd(Node.getContent_lengthKey, ' ')}` +
+        ` | ${params.dps}/${params.wds}`;
     }
 
     // footer
     // unknown params
 
-    const footer: string = inspect(params, [
-      'bal',
-      'dps',
-      'id',
-      'name',
-      'pnl',
-      'root',
-      'status',
-      'type',
-      'wds',
-    ]);
+    const footer: string = inspect(params, NodeParams_knownKeys);
 
     if (footer !== '') {
-      content += '\n' + '-'.repeat(18);
+      content += `\n${'-'.repeat(Node.getContent_lengthHeader)}`;
       content += footer;
     }
 
