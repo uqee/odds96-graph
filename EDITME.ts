12bb21caa537472fafc0f630b4025fcd8f001dc9
uqee <denis@zhbankov.com>
2023-07-28 22:58:36 +0000

    1 file modified

--- EDITME.ts
+++ EDITME.ts
@@ -5,99 +5,114 @@ export const clientInputs: ClientInput[] = [
     redash_fraudControl: `
     `,
     redash_fraudControl_account: `
+    342735
+    143atj@oksbi
+    payment_form
+    341442
+    143atj@oksbi
+    payment_form    
     `,
     redash_fraudControl_device: `
-    334334
-    1bd1f149-a2be-45f9-af6e-39fff331660a
+    350928
+    274dda16-2605-4596-974c-51679385af6c
+    341442
+    274dda16-2605-4596-974c-51679385af6c
+    346143
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64
+    346116
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64
     `,
     redash_fraudControl_email: `
-    334334
-    ritukaran395
-    kyc
     `,
     redash_fraudControl_name: `
-    137156
-    karandeepsingh
-    kyc
-    64330
-    karandeepsingh
-    payment_form
-    64330
-    karandeepsingh
-    kyc
     `,
     redash_fraudControl_password: `
     `,
     redash_fraudControl_phone: `
+    341374
+    919671976201
+    kyc
+    329202
+    917015901254
+    kyc
+    342735
+    919671976201
+    payment_form
+    342735
+    917015901254
+    kyc
+    341442
+    919671976201
+    payment_form
     `,
     retool_userInfo: `
     MAIN INFO
-    Client ID	334231
-    User ID	5688491380
-    Login	918168904264
-    Registration date(UTC)	12:06 13/07/23
-    Promocode	
+    Client ID	350890
+    User ID	4226687363
+    Login	pihuraja91@gmail.com
+    Registration date(UTC)	13:26 25/07/23
+    Promocode	null
     Country	IN
     Status	Active
     Social Login	✘
     IsTest	✘
     Is Trusted	✘
     Verification status	InProgress
-    Auto withdrawal score	1.0
-    Auto withdrawal factors	regular_clients_same_name, regular_clients_same_device, regular_clients_same_email
+    Auto withdrawal score	1.35
+    Auto withdrawal factors	regular_clients_same_name, live_fastbets_low_events_same_ip, phone_mismatch, regular_clients_same_phone, regular_clients_same_device, regular_clients_same_account_front, regular_clients_same_password, fraud_clients_same_name
     Client suspend score	0.5
     Client suspend factors	regular_clients_same_device
-    Pay to affiliate score	1.0
-    Pay to affiliate factors	no_pay
-    Loyalty Level	Starter
-    Segment Group	Starter
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Risk-taker
+    Segment Group	Challenger
     MTSProfile	
-    User 334231 MTS Profile
+    User 350890 MTS Profile
     CasinoLoyaltyProfile	
-    User 546322141
-    Firstname	Karandeep Singh
+    User 549220233
+    Firstname	null
     Lastname	null
-    Birthdate	15/02/91
-    Contact phone	918168904264
+    Birthdate	null
+    Contact phone	917015901254
     Phone Verified	✔
-    Contact email	ritukaran395@gmail.com
+    Contact email	pihuraja91@gmail.com
     Email Verified	✔
     Sumsub Profile	
-    User 64bf841eb8bbac160d5724c3
+    User 64c07d68c451e90ddd42d6c6
     STATISTICS
     Currency	INR
-    Total stakes (sports)	300 INR
-    Total stakes USD (sports)	$3.60
+    Total stakes (sports)	50 INR
+    Total stakes USD (sports)	$0.60
     No. of bets (sports)	1
     Unsettled sports stakes	0 INR
     Unsettled sports stakes USD	$0.00
     Sportsbook GGR	
-    300 INR
+    50 INR
     Sportsbook GGR USD	
-    $3.6
+    $0.6
     Casino GGR	
-    -106 INR
+    1292.07 INR
     Casino GGR USD	
-    $-1.27
-    Deposits	1200 INR
-    Deposits USD	$14.40
+    $15.5
+    Deposits	500 INR
+    Deposits USD	$6.00
     No. of deposits	1
     Withdrawals	0 INR
     Withdrawals USD	$0.00
     No. of withdrawals	0
     Fin Pnl	
-    1200 INR
+    500 INR
     Fin Pnl USD	
-    $14.4
+    $6
     Bookmaker fee	0
-    NGR Total	153 INR
-    NGR Total USD	$1.84
+    NGR Total	549.97 INR
+    NGR Total USD	$6.60
     Main balance	
     Currency	INR
-    Balance	1007
-    Balance USD	$12.08
-    NGR_total	153.00
-    NGR_total_USD	1.84    
+    Balance	0.03
+    Balance USD	$0.00
+    NGR_total	549.97
+    NGR_total_USD	6.60    
     `,
     text: ``,
   },
@@ -118,10 +133,10 @@ export const clientInputs: ClientInput[] = [
     `,
     retool_userInfo: `
     MAIN INFO
-    Client ID	334334
-    User ID	4723454176
-    Login	ritukaran395@gmail.com
-    Registration date(UTC)	13:46 13/07/23
+    Client ID	329202
+    User ID	9488452907
+    Login	917015901254
+    Registration date(UTC)	02:11 08/07/23
     Promocode	
     Country	IN
     Status	Active
@@ -138,16 +153,16 @@ export const clientInputs: ClientInput[] = [
     Loyalty Level	Novice
     Segment Group	New
     MTSProfile	
-    User 334334 MTS Profile
+    User 329202 MTS Profile
     CasinoLoyaltyProfile	
-    User 546337809
+    User 545005153
     Firstname	null
     Lastname	null
     Birthdate	null
-    Contact phone	null
-    Phone Verified	✘
-    Contact email	ritukaran395@gmail.com
-    Email Verified	✔
+    Contact phone	917015901254
+    Phone Verified	✔
+    Contact email	
+    Email Verified	✘
     Sumsub Profile	
     User
     STATISTICS
@@ -184,6 +199,631 @@ export const clientInputs: ClientInput[] = [
     Balance USD	$0.00
     NGR_total	-40.00
     NGR_total_USD	-0.48
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    `,
+    redash_fraudControl_device: `
+    247956
+    cc73d8e0-4e60-4d6e-b804-9164d1a5b59d
+    247956
+    dbaab258-b5f9-4078-a60b-46d5f4138a54
+    223861
+    d13f5698-de74-484f-9394-c368c1d16112
+    245481
+    62a78ae4-eca9-4cf2-820e-291151ab1b35
+    223861
+    cc063b14-2d87-4040-a6a6-68ea8ad40b37
+    247956
+    62a78ae4-eca9-4cf2-820e-291151ab1b35
+    245481
+    970b5505-d9ec-48da-8e18-5fbd8fa38f6f
+    347813
+    3b5beb2e-12bf-473d-af87-e8904a1c08fb
+    223861
+    26b2e4dc-8c13-4885-a40f-821d2eeb0933    `,
+    redash_fraudControl_email: `
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	341374
+    User ID	6818197525
+    Login	919671976201
+    Registration date(UTC)	13:05 19/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	Available
+    Auto withdrawal score	0.5
+    Auto withdrawal factors	regular_clients_same_device
+    Client suspend score	0.5
+    Client suspend factors	regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	New
+    MTSProfile	
+    User 341374 MTS Profile
+    CasinoLoyaltyProfile	
+    User 547858749
+    Firstname	null
+    Lastname	null
+    Birthdate	null
+    Contact phone	919671976201
+    Phone Verified	✔
+    Contact email	null
+    Email Verified	✘
+    Sumsub Profile	
+    User
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	50 INR
+    Total stakes USD (sports)	$0.60
+    No. of bets (sports)	1
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    50 INR
+    Sportsbook GGR USD	
+    $0.6
+    Casino GGR	
+    61.2 INR
+    Casino GGR USD	
+    $0.73
+    Deposits	0 INR
+    Deposits USD	$0.00
+    No. of deposits	0
+    Withdrawals	0 INR
+    Withdrawals USD	$0.00
+    No. of withdrawals	0
+    Fin Pnl	
+    0 INR
+    Fin Pnl USD	
+    $0
+    Bookmaker fee	0
+    NGR Total	50 INR
+    NGR Total USD	$0.60
+    Main balance	
+    Currency	INR
+    Balance	0
+    Balance USD	$0.00
+    NGR_total	50.00
+    NGR_total_USD	0.60
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    350890
+    143atj@oksbi
+    payment_form
+    342735
+    7602001500001613
+    payment_form
+    342735
+    143atj@oksbi
+    payment_form
+    `,
+    redash_fraudControl_device: `
+    350890
+    274dda16-2605-4596-974c-51679385af6c
+    345905
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    342735
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    329202
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    344307
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    345435
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    345359
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    346036
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    346661
+    4bf7d392-2c49-43e1-a8f5-3d763711249e    `,
+    redash_fraudControl_email: `
+    342956
+    143atj
+    kyc
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    341374
+    919671976201
+    kyc
+    350890
+    919671976201
+    payment_form
+    342735
+    919671976201
+    payment_form
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	341442
+    User ID	1623517067
+    Login	919813034071
+    Registration date(UTC)	13:47 19/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	Done
+    Auto withdrawal score	1.95
+    Auto withdrawal factors	regular_clients_same_name, same_ip, phone_mismatch, regular_clients_same_phone, regular_clients_same_device, regular_clients_same_account_front, regular_clients_same_email, casino_ngr_over_x, regular_clients_same_password, fraud_clients_same_name
+    Client suspend score	0.6
+    Client suspend factors	same_ip, regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	Starter
+    MTSProfile	
+    User 341442 MTS Profile
+    CasinoLoyaltyProfile	
+    User 547864481
+    Firstname	Akshay Kumar
+    Lastname	null
+    Birthdate	08/04/95
+    Contact phone	919813034071
+    Phone Verified	✔
+    Contact email	143atj@gmail.com
+    Email Verified	✔
+    Sumsub Profile	
+    User 64b7f78730f25402a7e28d10
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	60 INR
+    Total stakes USD (sports)	$0.72
+    No. of bets (sports)	2
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    60 INR
+    Sportsbook GGR USD	
+    $0.72
+    Casino GGR	
+    -1996.59 INR
+    Casino GGR USD	
+    $-23.96
+    Deposits	1900 INR
+    Deposits USD	$22.80
+    No. of deposits	3
+    Withdrawals	4000 INR
+    Withdrawals USD	$48.00
+    No. of withdrawals	2
+    Fin Pnl	
+    -2100 INR
+    Fin Pnl USD	
+    $-25.2
+    Bookmaker fee	0
+    NGR Total	-2050.09 INR
+    NGR Total USD	-$24.60
+    Main balance	
+    Currency	INR
+    Balance	0.09
+    Balance USD	$0.00
+    NGR_total	-2050.09
+    NGR_total_USD	-24.60
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    350890
+    143atj@oksbi
+    payment_form
+    341442
+    7602001500001613
+    payment_form
+    341442
+    143atj@oksbi
+    payment_form
+    `,
+    redash_fraudControl_device: `
+    345905
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    341442
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    329202
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    344307
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    345435
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    345359
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    346036
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    346661
+    4bf7d392-2c49-43e1-a8f5-3d763711249e
+    346066
+    4bf7d392-2c49-43e1-a8f5-3d763711249e    `,
+    redash_fraudControl_email: `
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    341374
+    919671976201
+    kyc
+    329202
+    917015901254
+    kyc
+    350890
+    919671976201
+    payment_form
+    350890
+    917015901254
+    kyc
+    341442
+    919671976201
+    payment_form
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	342735
+    User ID	7472623852
+    Login	akshaykumar5538p@gmail.com
+    Registration date(UTC)	10:38 20/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	InProgress
+    Auto withdrawal score	1.0
+    Auto withdrawal factors	regular_clients_same_name, phone_mismatch, regular_clients_same_phone, regular_clients_same_device, regular_clients_same_account_front, fraud_clients_same_name
+    Client suspend score	0.5
+    Client suspend factors	regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	Starter
+    MTSProfile	
+    User 342735 MTS Profile
+    CasinoLoyaltyProfile	
+    User 548057045
+    Firstname	Akshay Kumar
+    Lastname	null
+    Birthdate	08/04/95
+    Contact phone	917015901254
+    Phone Verified	✔
+    Contact email	akshaykumar5538p@gmail.com
+    Email Verified	✔
+    Sumsub Profile	
+    User 64b91c3a099b857c48bd2535
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	50 INR
+    Total stakes USD (sports)	$0.60
+    No. of bets (sports)	1
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    50 INR
+    Sportsbook GGR USD	
+    $0.6
+    Casino GGR	
+    655.3 INR
+    Casino GGR USD	
+    $7.86
+    Deposits	500 INR
+    Deposits USD	$6.00
+    No. of deposits	1
+    Withdrawals	0 INR
+    Withdrawals USD	$0.00
+    No. of withdrawals	0
+    Fin Pnl	
+    500 INR
+    Fin Pnl USD	
+    $6
+    Bookmaker fee	0
+    NGR Total	550 INR
+    NGR Total USD	$6.60
+    Main balance	
+    Currency	INR
+    Balance	0
+    Balance USD	$0.00
+    NGR_total	550.00
+    NGR_total_USD	6.60    
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    `,
+    redash_fraudControl_device: `
+    341442
+    14821bc1-aa45-4453-93a4-a8700cc13420
+    346143
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64
+    329202
+    14821bc1-aa45-4453-93a4-a8700cc13420
+    350890
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64    `,
+    redash_fraudControl_email: `
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	346116
+    User ID	2469573800
+    Login	pawaranup972@gmail.com
+    Registration date(UTC)	14:58 21/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	Available
+    Auto withdrawal score	0.7
+    Auto withdrawal factors	regular_clients_same_device, regular_clients_same_password
+    Client suspend score	0.5
+    Client suspend factors	regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	New
+    MTSProfile	
+    User 346116 MTS Profile
+    CasinoLoyaltyProfile	
+    User 548335329
+    Firstname	null
+    Lastname	null
+    Birthdate	null
+    Contact phone	null
+    Phone Verified	✘
+    Contact email	pawaranup972@gmail.com
+    Email Verified	✔
+    Sumsub Profile	
+    User
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	0 INR
+    Total stakes USD (sports)	$0.00
+    No. of bets (sports)	0
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    0 INR
+    Sportsbook GGR USD	
+    $0
+    Casino GGR	
+    224.92 INR
+    Casino GGR USD	
+    $2.7
+    Deposits	0 INR
+    Deposits USD	$0.00
+    No. of deposits	0
+    Withdrawals	0 INR
+    Withdrawals USD	$0.00
+    No. of withdrawals	0
+    Fin Pnl	
+    0 INR
+    Fin Pnl USD	
+    $0
+    Bookmaker fee	0
+    NGR Total	-0.98 INR
+    NGR Total USD	-$0.01
+    Main balance	
+    Currency	INR
+    Balance	0.98
+    Balance USD	$0.01
+    NGR_total	-0.98
+    NGR_total_USD	-0.01
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    `,
+    redash_fraudControl_device: `
+    346116
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64
+    350890
+    3f847dd9-42f6-4a47-a9c2-f03f25324a64    `,
+    redash_fraudControl_email: `
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	346143
+    User ID	5137962903
+    Login	a22426525@gmail.com
+    Registration date(UTC)	15:23 21/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	Available
+    Auto withdrawal score	0.7
+    Auto withdrawal factors	regular_clients_same_device, regular_clients_same_password
+    Client suspend score	0.5
+    Client suspend factors	regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	New
+    MTSProfile	
+    User 346143 MTS Profile
+    CasinoLoyaltyProfile	
+    User 548339585
+    Firstname	null
+    Lastname	null
+    Birthdate	null
+    Contact phone	null
+    Phone Verified	✘
+    Contact email	a22426525@gmail.com
+    Email Verified	✔
+    Sumsub Profile	
+    User
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	0 INR
+    Total stakes USD (sports)	$0.00
+    No. of bets (sports)	0
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    0 INR
+    Sportsbook GGR USD	
+    $0
+    Casino GGR	
+    104.08 INR
+    Casino GGR USD	
+    $1.25
+    Deposits	0 INR
+    Deposits USD	$0.00
+    No. of deposits	0
+    Withdrawals	0 INR
+    Withdrawals USD	$0.00
+    No. of withdrawals	0
+    Fin Pnl	
+    0 INR
+    Fin Pnl USD	
+    $0
+    Bookmaker fee	0
+    NGR Total	-0.12 INR
+    NGR Total USD	$0.00
+    Main balance	
+    Currency	INR
+    Balance	0.12
+    Balance USD	$0.00
+    NGR_total	-0.12
+    NGR_total_USD	0.00
+    `,
+    text: ``,
+  },
+  {
+    redash_fraudControl: `
+    `,
+    redash_fraudControl_account: `
+    `,
+    redash_fraudControl_device: `
+    350890
+    274dda16-2605-4596-974c-51679385af6c
+    341442
+    274dda16-2605-4596-974c-51679385af6c    `,
+    redash_fraudControl_email: `
+    `,
+    redash_fraudControl_name: `
+    `,
+    redash_fraudControl_password: `
+    `,
+    redash_fraudControl_phone: `
+    `,
+    retool_userInfo: `
+    MAIN INFO
+    Client ID	350928
+    User ID	9115838668
+    Login	akkusingh89677@gmail.com
+    Registration date(UTC)	14:06 25/07/23
+    Promocode	null
+    Country	IN
+    Status	Active
+    Social Login	✘
+    IsTest	✘
+    Is Trusted	✘
+    Verification status	Available
+    Auto withdrawal score	0.85
+    Auto withdrawal factors	live_fastbets_low_events_same_ip, regular_clients_same_device, regular_clients_same_password
+    Client suspend score	0.5
+    Client suspend factors	regular_clients_same_device
+    Pay to affiliate score	0.0
+    Pay to affiliate factors	
+    Loyalty Level	Starter
+    Segment Group	New
+    MTSProfile	
+    User 350928 MTS Profile
+    CasinoLoyaltyProfile	
+    User 549226241
+    Firstname	null
+    Lastname	null
+    Birthdate	null
+    Contact phone	null
+    Phone Verified	✘
+    Contact email	akkusingh89677@gmail.com
+    Email Verified	✔
+    Sumsub Profile	
+    User
+    STATISTICS
+    Currency	INR
+    Total stakes (sports)	50 INR
+    Total stakes USD (sports)	$0.60
+    No. of bets (sports)	1
+    Unsettled sports stakes	0 INR
+    Unsettled sports stakes USD	$0.00
+    Sportsbook GGR	
+    50 INR
+    Sportsbook GGR USD	
+    $0.6
+    Casino GGR	
+    42.8 INR
+    Casino GGR USD	
+    $0.51
+    Deposits	0 INR
+    Deposits USD	$0.00
+    No. of deposits	0
+    Withdrawals	0 INR
+    Withdrawals USD	$0.00
+    No. of withdrawals	0
+    Fin Pnl	
+    0 INR
+    Fin Pnl USD	
+    $0
+    Bookmaker fee	0
+    NGR Total	49.9 INR
+    NGR Total USD	$0.60
+    Main balance	
+    Currency	INR
+    Balance	0.1
+    Balance USD	$0.00
+    NGR_total	49.90
+    NGR_total_USD	0.60
     `,
     text: ``,
   },
